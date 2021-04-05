5ffe107c28b6c7019522cf6a2cfc9bd5f53a8fe6
ylq-67 <81059202+ylq-67@users.noreply.github.com>
2021-04-05 21:24:41 +0800

    java训练营第三周作业（必做）

---
+++ week03/HttpHandler.java
@@ -0,0 +1,187 @@
+package io.github.ylq.extendNettyServer;
+
+
+import io.github.kimmking.gateway.filter.HeaderHttpResponseFilter;
+import io.github.kimmking.gateway.filter.HttpResponseFilter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderUtil;
+//import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+    private CloseableHttpAsyncClient httpclient;
+    HttpRequestFilter filter = new HeaderHttpRequestFilter();
+
+
+    public HttpHandler() {
+        int cores = Runtime.getRuntime().availableProcessors();
+        long keepAliveTime = 1000;
+        int queueSize = 2048;
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();//.DiscardPolicy();
+//        proxyService = new ThreadPoolExecutor(cores, cores,
+//                keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
+//                new NamedThreadFactory("proxyService"), handler);
+
+        IOReactorConfig ioConfig = IOReactorConfig.custom()
+                .setConnectTimeout(1000)
+                .setSoTimeout(1000)
+                .setIoThreadCount(cores)
+                .setRcvBufSize(32 * 1024)
+                .build();
+
+        httpclient = HttpAsyncClients.custom().setMaxConnTotal(40)
+                .setMaxConnPerRoute(8)
+                .setDefaultIOReactorConfig(ioConfig)
+                .setKeepAliveStrategy((response,context) -> 6000)
+                .build();
+        httpclient.start();
+    }
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                filter.filter(fullRequest, ctx);
+                handlerTest(fullRequest, ctx);
+            }
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+//        try {
+//            String value = "hello,ylq"; // "hello,kimmking"; // 对接上次作业的httpclient或者okhttp请求另一个url的响应数据
+
+//            httpGet ...  http://localhost:8801
+//            返回的响应，"hello,nio";
+//            value = reponse....
+
+//            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+//            response.headers().set("Content-Type", "application/json");
+//            response.headers().setInt("Content-Length", response.content().readableBytes());
+//
+//        } catch (Exception e) {
+//            System.out.println("处理出错:"+e.getMessage());
+//            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+//        } finally {
+//            if (fullRequest != null) {
+//                if (!HttpHeaderUtil.isKeepAlive(fullRequest)) {
+//                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+//                } else {
+//                    response.headers().set(CONNECTION, KEEP_ALIVE);
+//                    ctx.write(response);
+//                }
+//            }
+//        }
+
+        HttpGet httpGet = new HttpGet("http://localhost:8088");
+        httpGet.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_KEEP_ALIVE);
+        httpGet.setHeader("mao-ylq", (String) fullRequest.headers().get("mao-ylq"));
+
+        System.out.println((String) fullRequest.headers().get("mao-ylq"));
+        for (Header e : httpGet.getAllHeaders()) {
+            System.out.println(e.getName() + " => " + e.getValue());
+        }
+
+        httpclient.execute(httpGet, new FutureCallback<HttpResponse>() {
+            @Override
+            public void completed(final HttpResponse endpointResponse) {
+                try {
+                    handleResponse(fullRequest, ctx, endpointResponse);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+
+            @Override
+            public void failed(final Exception ex) {
+                httpGet.abort();
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void cancelled() {
+                httpGet.abort();
+            }
+        });
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final HttpResponse endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+
+        try {
+            byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", Integer.parseInt(endpointResponse.getFirstHeader("Content-Length").getValue()));
+
+//            for (Header e : endpointResponse.getAllHeaders()) {
+//                //response.headers().set(e.getName(),e.getValue());
+//                System.out.println(e.getName() + " => " + e.getValue());
+//            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpHeaderUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    //response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+            //ctx.close();
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
